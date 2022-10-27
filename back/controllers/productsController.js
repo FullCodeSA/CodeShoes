29ed2c8bec51ee7b207c1a3ad0a9574e7bc05378
LuFeBoAv <110979313+LuFeBoAv@users.noreply.github.com>
2022-10-27 01:11:15 -0500

    front de la tienda

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -8,13 +8,77 @@ exports.getProducts=(req,res,next) =>{
 
 //VER LISTA PRODUCTOS
 exports.getProducts=async (req,res,next) =>{
-        res.status(200).json({
-            success:true,
-            message:"En esta ruta usted v a ver todos los productos"
+    const productos= await producto.find();
+    if (!productos){
+        return res.status(404).json({
+            success:false,
+            error:true
         })
+    }
+
+    res.status(200).json({
+        success:true,
+        cantidad: productos.length,
+        productos
+    })
 }
 
+//VER PRODUCTO POR ID
+exports.getProductById= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
+    
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No encontramos ese producto',
+            error:true
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"Aqui debajo encuentras información sobre tu producto: ",
+        product
+    })
+}
 
+//UPDATE UN PRODUCTO
+exports.updateProduct= async (req,res,next) =>{
+    let product= await producto.findById(req.params.id) 
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No encontramos ese producto'
+        })
+    }
+    //SI EJECUTA, SE ACTUALIZA
+    product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators:true
+    });
+    //RESPUESTA SI SE ACTUALIZA OK
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
+
+//ELIMINAR PRODUCTO
+exports.deleteProduct= async (req,res,next) =>{
+    const product= await producto.findById(req.params.id)
+    if (!product){
+            return res.status(404).json({ 
+            success:false,
+            message: 'No encontramos ese producto'
+        })
+    }
+
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente"
+    })
+}
 //CREAR NUEVO PRODUCTO /API/PRODUCTOS
 exports.newProduct =async(req,res,next)=>{
     const product = await producto.create(req.body);
@@ -24,3 +88,4 @@ exports.newProduct =async(req,res,next)=>{
         product
     })
 }
+
---
+++ front/src/components/layout/Header.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react'
+
+const Header = () => {
+  return (
+    <Fragment>
+         <nav class="navbar">
+    <img src="./img/CodeShoesH.png" class="brand-logo" alt=""></img>
+    <ul class="nav-links">
+        <li class="nav-items"><a href="http://localhoost:3000">!!!!</a></li>
+        <li class="nav-items"><a href="http://localhoost:3000">$$$</a></li>
+        <li class="nav-items"><a href="http://localhoost:3000">%%%%</a></li>
+        <li class="nav-items"><a href="http://localhoost:3000">????</a></li>
+    </ul>
+
+    <div class="right-container">
+        <input type="text" class="search-box" placeholder="search"></input>
+        <button class="sub-btn">Registrese</button>
+        <a href="http://localhoost:3000" class="login-link">Inicio Sesión</a>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
+        <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="false"></i>
+        <span class="nav-items"  id="cart_count">2</span>
+    </div>
+</nav>
+    </Fragment>
+  )
+}
+
+export default Header
